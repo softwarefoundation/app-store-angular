76aa29a400ade8a6f6d151ab43026ff8cab71cc0
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-12-29 05:12:12 -0400

    Add componente product crud

--- app-store-angular-web/src/app/app.module.ts
+++ app-store-angular-web/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MatSidenavModule} from '@angular/material/sidenav';
 import { MatListModule} from '@angular/material/list';
 import { MatCardModule} from '@angular/material/card';
 import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { HomeComponent } from './views/home/home.component';
     HeaderComponent,
     FooterComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    ProductCrudComponent
   ],
   imports: [
     BrowserModule,
