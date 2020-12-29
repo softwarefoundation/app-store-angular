316b07ddd8a292762884e741f3abca202a842bfd
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-12-29 05:04:08 -0400

    Add componente home

--- app-store-angular-web/src/app/app.module.ts
+++ app-store-angular-web/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 import { MatSidenavModule} from '@angular/material/sidenav';
 import { MatListModule} from '@angular/material/list';
+import { MatCardModule} from '@angular/material/card';
+import { HomeComponent } from './views/home/home.component';
 
 
 @NgModule({
@@ -17,7 +19,8 @@ import { MatListModule} from '@angular/material/list';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +28,8 @@ import { MatListModule} from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
