c3bdfbe7553bc8a9b1ef617215b9a99059c4ba6c
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-12-30 03:59:21 -0400

    Add component ProductCreate

--- app-store-angular-web/src/app/app-routing.module.ts
+++ app-store-angular-web/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent} from './views/home/home.component'
@@ -10,6 +11,10 @@ const routes: Routes = [{
 {
   path: "products",
   component: ProductCrudComponent
+},
+{
+  path: "products/create",
+  component: ProductCreateComponent
 }
 ];
 
