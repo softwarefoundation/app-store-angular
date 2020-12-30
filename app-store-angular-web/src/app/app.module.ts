c7641d2ea4f8baf06975c63de3565cbcbb38c123
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-12-30 04:16:10 -0400

    Add botão de navegação e chamada de função

--- app-store-angular-web/src/app/app.module.ts
+++ app-store-angular-web/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { NavComponent } from './components/template/nav/nav.component';
 import { MatSidenavModule} from '@angular/material/sidenav';
 import { MatListModule} from '@angular/material/list';
 import { MatCardModule} from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
@@ -33,7 +34,8 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
