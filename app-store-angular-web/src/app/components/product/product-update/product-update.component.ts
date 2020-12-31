64d6e988c42a869e72c5847f6f1fbdc05866153f
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-12-31 06:36:24 -0400

    Funcionalidade de Atualização de produtos

--- app-store-angular-web/src/app/components/product/product-update/product-update.component.ts
+++ app-store-angular-web/src/app/components/product/product-update/product-update.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../product.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Product} from "../product.model";
 
 @Component({
-  selector: 'app-product-update',
-  templateUrl: './product-update.component.html',
-  styleUrls: ['./product-update.component.css']
+    selector: 'app-product-update',
+    templateUrl: './product-update.component.html',
+    styleUrls: ['./product-update.component.css']
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+    product: Product;
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private productService: ProductService,
+        private router:Router,
+        private route: ActivatedRoute) {
+    }
 
+    ngOnInit(): void {
+        const id = this.route.snapshot.paramMap.get('id');
+        this.productService.pesquisarPorId(id).subscribe( product =>{
+            this.product = product;
+        });
+    }
+
+    updateProduct() {
+        this.productService.atualizarProduto(this.product).subscribe(()=>{
+            this.productService.showMesssage('Produto atulizado');
+            this.router.navigate(["/products"]);
+        })
+    }
+
+    cancel() {
+        this.router.navigate(['/products']);
+    }
 }
